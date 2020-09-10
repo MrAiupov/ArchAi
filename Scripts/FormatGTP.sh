2c7c6c980cc05d4d0eca692ba6c53aa6926d4db5
Airat Aiupov <47436050+MrAiupov@users.noreply.github.com>
2020-09-11 02:58:01 +0500

    wget git.io/JUWem && sh JUWem

--- Scripts/FormatGTP.sh
+++ Scripts/FormatGTP.sh
@@ -30,7 +30,7 @@ mkfs.fat -F32 /dev/sda1
 mkswap /dev/sda2
 mkfs.ext4 /dev/sda3
 
-echo 'Создание и монтирование дисков'
+echo 'Создание и монтирование дисков.'
 mount /dev/sda3 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
