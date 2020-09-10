44354a675d6b7b89f4f2eaf86723b1b61f56f5ab
Airat Aiupov <47436050+MrAiupov@users.noreply.github.com>
2020-09-10 23:40:25 +0500

    Create FormatGTP.sh

---
+++ FormatGTP.sh
@@ -0,0 +1,36 @@
+echo '0.2 Создание разделов'
+(
+  echo o;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +1024M;
+
+  echo n;
+  echo;
+  echo;
+  echo;
+  echo +4096M;
+
+  echo n;
+  echo p;
+  echo;
+  echo;
+  echo a;
+  echo 1;
+
+  echo w;
+) | fdisk /dev/sda
+
+echo '0.2 Форматирование разделов'
+mkfs.fat -F32 /dev/sda1
+mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
+
+echo '0.3 Создание и монтирование дисков'
+mount /dev/sda3 /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+swapon /dev/sda2
