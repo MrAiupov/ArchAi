dcae5209aecef993a2adcfc60d46c96fc9952ef7
Airat Aiupov <47436050+MrAiupov@users.noreply.github.com>
2020-09-11 03:53:46 +0500

    Create AUR.sh

---
+++ Scripts/AUR.sh
@@ -0,0 +1,13 @@
+echo 'Включение AUR репозитория'
+echo 'Включение AUR репозитория'
+sudo pacman -Syu
+mkdir -p /tmp/yay_install
+cd /tmp/yay_install
+sudo pacman -S git --noconfirm
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -sir --needed --noconfirm --skippgpcheck
+cd ..
+cd ..
+rm -rf yay_install
+cd ..
