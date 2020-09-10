0657b2908e2ec9a39027ab023be46dbf3b2a91d0
Airat Aiupov <47436050+MrAiupov@users.noreply.github.com>
2020-09-10 23:34:44 +0500

    Update GPT.sh

--- GPT.sh
+++ GPT.sh
@@ -1,1 +1,4 @@
-
+echo '1.4.1 Устанавливаем загрузчик для GPT'
+pacman -Syy
+pacman -S grub efibootmgr os-prober grub-customizer --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
