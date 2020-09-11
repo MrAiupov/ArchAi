cd41e97bf528b2999d0b36d2ceec3590af8fe064
Airat Aiupov <47436050+MrAiupov@users.noreply.github.com>
2020-09-11 05:16:14 +0500

    wget git.io/JUWf6 && sh JUWf6

--- Scripts/AUR.sh
+++ Scripts/AUR.sh
@@ -1,12 +1,12 @@
-echo 'wget git.io/JUWf6 && sh JUWf6'
+# wget git.io/JUWf6 && sh JUWf6
 echo 'Включение AUR репозитория'
 sudo pacman -Syu
 mkdir -p /tmp/yay_install
 cd /tmp/yay_install
-sudo pacman -S git --noconfirm
+sudo pacman -S wget git curl --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -sir --needed --noconfirm --skippgpcheck
+makepkg -si --skipinteg
 cd ..
 cd ..
 rm -rf yay_install
