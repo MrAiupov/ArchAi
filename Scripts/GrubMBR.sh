1041dff04d697b6cb4fdf8b206d1673677116612
Airat Aiupov <47436050+MrAiupov@users.noreply.github.com>
2020-09-11 19:28:04 +0500

    wget git.io/JUWeg && sh JUWeg

--- Scripts/GrubMBR.sh
+++ Scripts/GrubMBR.sh
@@ -1,4 +1,6 @@
-echo 'wget git.io/JUWeg && sh JUWeg'
+#!/bin/bash
+
+# wget git.io/JUWeg && sh JUWeg
 echo 'Устанавливаем загрузчик для MBR'
 pacman -Syy
 pacman -S grub os-prober grub-customizer --noconfirm 
